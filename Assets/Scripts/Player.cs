4d60d2b35922882350f4a01c4117307d35d80138
James Petit <jp.james.petit@gmail.com>
2016-04-07 14:52:39 +0200

    Version de base.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour 
+{
+	[HideInInspector]public string playerName;
+	[HideInInspector]public int playerNumber;
+	[HideInInspector]public int hitPoints = 2;
+	[HideInInspector]public int storedMoney = 0;
+	[HideInInspector]public int currentMoney = 0;
+	[HideInInspector]public int moneyGiven = 0;
+	[HideInInspector]public List<string> items = new List<string>();
+	public GameManager game;
+	
+	void Update () 
+	{
+		if(hitPoints == 0)
+		{
+			Death ();
+		}
+	}
+
+	void Death()
+	{
+		currentMoney = 0;
+		game.playersRemaining--;
+	}
+
+	void Stop()
+	{
+		storedMoney += (currentMoney + game.commonMoney);
+		game.commonMoney = 0;
+		currentMoney = 0;
+		game.playersRemaining--;
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UIManager : MonoBehaviour {
+
+    public bool Player1_Continue;
+    public bool Player2_Continue;
+    public bool Player3_Continue;
+    public bool Player4_Continue;
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+       
+
+	}
+
+    public void Continuer(int Player)
+    {
+        switch (Player)
+        {
+            case 0:
+                break;
+
+            case 1:
+                Player1_Continue = true;
+                break;
+
+            case 2:
+                Player2_Continue = true;
+                break;
+
+            case 3:
+                Player3_Continue = true;
+                break;
+
+            case 4:
+                Player4_Continue = true;
+                break;
+        }          
+       
+    }
+
+    public void Rentrer(int Player)
+    {
+        switch (Player)
+        {
+            case 0:
+                break;
+
+            case 1:
+                Player1_Continue = false;
+                break;
+
+            case 2:
+                Player2_Continue = false;
+                break;
+
+            case 3:
+                Player3_Continue = false;
+                break;
+
+            case 4:
+                Player4_Continue = false;
+                break;
+        }
+    }
+
+
+}
